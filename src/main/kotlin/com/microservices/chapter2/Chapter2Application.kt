294eff7bc89eb01f8a6da382a2c8ef94736ca28a
user <user@email.com>
2021-07-27 18:09:59 +0900

    service example

--- src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -3,6 +3,7 @@ package com.microservices.chapter2
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody
 class Chapter2Application
 
 @Controller
-class FirstController {
-	@RequestMapping(value=["/user"], method=[RequestMethod.GET])
+class FirstController(val exampleService: ExampleService) {
+	@RequestMapping(value=["/user/{name}"], method=[RequestMethod.GET])
 	@ResponseBody
-	fun hello() = "hello world!"
+	fun hello(
+			@PathVariable
+			name: String
+	) = exampleService.getHello(name)
 }
 
 fun main(args: Array<String>) {
